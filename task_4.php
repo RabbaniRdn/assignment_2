c75388824cd748750279ef58ed332ac144d91247
Rabbani <rabbanirdr@gmail.com>
2023-09-20 23:51:44 +0600

    added

--- task_4.php
+++ task_4.php
@@ -4,7 +4,7 @@ function fibonacci ($number){
     $old=0;
     $new=1;
     for ($i=1;$i<=$number;$i++){
-        echo "{$old}\n";
+        echo "{$old}\n";git branch -M main
         $temp=$old+$new;
         $old=$new;
         $new=$temp;
